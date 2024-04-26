253928847e58cedb49e8e9f5ca494ee88da1c6c4
Aprameya Shankar <aprameyashankar@gmail.com>
2024-04-26 10:56:43 +0530

    Added Symmetrical Transposition Algo

--- client/src/lib/AES.jsx
+++ client/src/lib/AES.jsx
@@ -104,12 +104,19 @@ Aes.subBytes = function(s, Nb) {
  * @private
  */
 Aes.shiftRows = function(s, Nb) {
-    var t = new Array(4);
-    for (var r=1; r<4; r++) {
-        for (var c=0; c<4; c++) t[c] = s[r][(c+r)%Nb];  // shift into temp copy
-        for (var c=0; c<4; c++) s[r][c] = t[c];         // and copy back
-    }          // note that this will work for Nb=4,5,6, but not 7,8 (always 4 for AES):
-    return s;  // see asmaes.sourceforge.net/rijndael/rijndaelImplementation.pdf
+    // var t = new Array(4);
+    // for (var r=1; r<4; r++) {
+    //     for (var c=0; c<4; c++) t[c] = s[r][(c+r)%Nb];  // shift into temp copy
+    //     for (var c=0; c<4; c++) s[r][c] = t[c];         // and copy back
+    // }          // note that this will work for Nb=4,5,6, but not 7,8 (always 4 for AES):
+    // return s;  // see asmaes.sourceforge.net/rijndael/rijndaelImplementation.pdf
+
+    // Symmetrical Transposition Method
+    s = s.map((_, colIndex) => s.map(row => row[colIndex]));
+    s[0][0]=[s[3][3],s[3][3]=s[0][0]][0]
+    s[1][1]=[s[2][2],s[2][2]=s[1][1]][0]
+
+    return s
 };
 
 
